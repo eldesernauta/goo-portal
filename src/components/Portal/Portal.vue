eede033eb64a22bc39a20e343247dc082cae7c76
eldesernauta <oscarrojascs@gmail.com>
2025-02-10 18:38:29 -0500

    links

--- src/components/Portal/Portal.vue
+++ src/components/Portal/Portal.vue
@@ -5,23 +5,31 @@
   >
     <!-- Avatar y Nombre -->
     <div class="text-center mb-8 fade-in">
-      <img src="/src/assets/icon.png" alt="Avatar" class="w-[100px] mx-auto" />
+      <img
+        src="/src/assets/icon.png"
+        alt="Avatar"
+        class="w-[100px] mx-auto scale-100 hover:scale-110 transition-transform duration-1000 ease-out hover:animate-pulse"
+      />
       <h1 class="mt-4 mb-2 text-4xl font-bold">Goo Hub</h1>
       <p class="text-blue-goo">Agencia de Marketing Digital</p>
     </div>
 
     <!-- Enlaces -->
-    <div class="w-full max-w-md flex flex-col gap-4 slide-up">
-      <a
-        v-for="(link, index) in links"
-        :key="index"
-        :href="link.url"
-        target="_blank"
-        class="block w-full bg-[rgba(255,255,255,0.3)] hover:bg-[#9735ED] text-black hover:text-white text-center py-3 rounded-lg font-semibold shadow-md hover:scale-105 transition-all duration-300"
-      >
-        {{ link.text }}
-      </a>
-    </div>
+    <a
+      v-for="(link, index) in links"
+      :key="index"
+      :href="link.url"
+      target="_blank"
+      :style="{
+        '--hover-color': link.bgColor,
+      }"
+      class="block w-full max-w-md my-2 text-sm text-white hover:text-black text-center py-3 rounded-lg shadow-md hover:scale-105 transition-all duration-300"
+      :class="{
+        'bg-[rgba(255,255,255,0.2)] hover:bg-[var(--hover-color)]': true,
+      }"
+    >
+      {{ link.text }}
+    </a>
 
     <!-- Teléfonos -->
     <div class="w-full max-w-md flex mt-8 slide-up">
@@ -29,12 +37,13 @@
         class="relative overflow-visible mx-auto mb-4 inline-flex items-center justify-center gap-4 group"
       >
         <div
-          class="absolute overflow-visible inset-0 duration-1000 opacity-60 transitiona-all bg-gradient-to-r from-indigo-500 via-pink-500 to-yellow-400 rounded-full blur-lg filter group-hover:opacity-100 group-hover:duration-200"
+          class="absolute overflow-visible inset-0 duration-1000 opacity-60 transitiona-all bg-gradient-to-r from-[#9735ED] via-[#67F4B8] to-[#FCFC8B] rounded-full blur-lg filter group-hover:opacity-100 group-hover:duration-200"
         ></div>
         <a
           role="button"
           class="group relative inline-flex items-center justify-center text-base rounded-full bg-gray-900 hover:bg-[#3E2A4C] px-8 py-3 font-semibold text-white transition-all duration-200 hover:shadow-lg hover:-translate-y-0.5 hover:shadow-gray-600/30"
           title="Whatsapp"
+          target="_blank"
           href="#"
           >¡Hablemos!<svg
             aria-hidden="true"
@@ -78,6 +87,7 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
 import { onMounted } from "vue";
 import gsap from "gsap";
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -94,26 +104,56 @@ import {
 // Agregar íconos a la librería
 library.add(faEnvelope, faFacebookF, faLinkedin, faInstagram);
 
-const links = [
-  { text: "Sitio Web", url: "https://goohub.digital" },
-  { text: "Servicios", url: "https://goohub.digital/servicios" },
-  { text: "Portafolio", url: "https://goohub.digital/portafolio" },
-  { text: "Contacto", url: "https://goohub.digital/contacto" },
-];
+const links = ref([
+  {
+    text: "Sitio Web",
+    bgColor: "#9735ED",
+    url: "https://dev.goohub.digital",
+  },
+  {
+    text: "Servicios",
+    bgColor: "#FCFC8B",
+    url: "https://dev.goohub.digital/servicios",
+  },
+  {
+    text: "Portafolio",
+    bgColor: "#67F4B8",
+    url: "https://dev.goohub.digital/portafolio",
+  },
+  {
+    text: "Contacto",
+    bgColor: "#6EBBF9",
+    url: "https://dev.goohub.digital/contacto",
+  },
+]);
 
 const contacts = [{ text: "+57 350 227 2625", url: "tel:+573003303330" }];
 
 const socialLinks = [
-  { icon: faInstagram, url: "https://instagram.com/goohubdigital" },
-  { icon: faFacebookF, url: "https://facebook.com/goohubdigital" },
-  { icon: faLinkedin, url: "https://linkedin.com/company/goohubdigital" },
-  { icon: faEnvelope, url: "mailto:info@goohub.digital" },
+  {
+    icon: faInstagram,
+    url: "https://instagram.com/goohub.digital",
+  },
+  {
+    icon: faFacebookF,
+    url: "https://facebook.com/goohub.digital",
+  },
+  {
+    icon: faLinkedin,
+    url: "https://linkedin.com/company/goohub-digital",
+  },
+  {
+    icon: faEnvelope,
+    url: "mailto:info@goohub.digital",
+  },
 ];
 
 onMounted(() => {
   gsap.from(".fade-in", { opacity: 0, duration: 1, y: -20 });
   gsap.from(".slide-up", { opacity: 0, duration: 1, y: 20, stagger: 0.2 });
 });
+
+const getBgColor = (bgColor) => bgColor || "rgba(255,255,255,0.3)";
 </script>
 
 <style scoped>
